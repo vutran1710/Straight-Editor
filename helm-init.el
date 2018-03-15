3a3ca446b64a2737357f441b664b88d24336ba58
vutran47 <vutran.1710@gmail.com>
2018-03-15 23:49:20 +0700

    Add helm-themes

--- helm-init.el
+++ helm-init.el
@@ -1,12 +1,14 @@
 (require 'helm)
 (require 'helm-ag)
 (require 'helm-config)
+(require 'helm-themes)
 (require 'swiper-helm)
 
 (global-unset-key "\M-x")
 (global-set-key (kbd "M-m") 'helm-M-x)
 (global-set-key (kbd "C-s") 'swiper-helm)
 (global-set-key (kbd "C-c r") 'helm-recentf)
+(global-set-key (kbd "C-c t") 'helm-themes)
 
 
 (provide 'may-helm)
