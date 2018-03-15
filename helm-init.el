33daa16677c804e0af68ba3060bd93e361a92ce1
vutran47 <vutran.1710@gmail.com>
2018-03-15 10:02:12 +0700

    Update

--- helm-init.el
+++ helm-init.el
@@ -1,8 +1,10 @@
 (require 'helm)
 (require 'helm-ag)
 (require 'helm-config)
+(require 'swiper-helm)
 
 (global-unset-key "\M-x")
 (global-set-key (kbd "M-m") 'helm-M-x)
+(global-set-key (kbd "C-s") 'swiper-helm)
 
 (provide 'may-helm)
--- keybindings.el
+++
@@ -1,1 +0,0 @@
-(global-set-key (kbd "C-s") 'swiper-helm)
