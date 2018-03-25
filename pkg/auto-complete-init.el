09f02042ceedef30423abb6b39686c61a41a5d43
vutran47 <vutran.1710@gmail.com>
2018-03-26 00:12:39 +0700

    Change auto-complete triggger key

--- pkg/auto-complete-init.el
+++ pkg/auto-complete-init.el
@@ -2,11 +2,11 @@
 (require 'auto-complete-config)
 
 (ac-ropemacs-initialize)
-(setq ac-auto-start nil)
-(ac-set-trigger-key "TAB")
+;; (setq ac-auto-start nil)
+;; (ac-set-trigger-key "TAB")
 
-;; (define-key ac-complete-mode-map "\t" 'ac-expand)
-;; (define-key ac-complete-mode-map "\r" 'ac-complete)
+(define-key ac-complete-mode-map "\t" 'ac-expand)
+(define-key ac-complete-mode-map "\r" 'ac-complete)
 (define-key ac-complete-mode-map "\M-n" 'ac-next)
 (define-key ac-complete-mode-map "\M-p" 'ac-previous)
 
@@ -17,8 +17,3 @@
                            (auto-complete-mode 1))
                        ))
 (real-global-auto-complete-mode t)
-
-(setq ac-modes
-      (append ac-modes
-              '(eshell-mode rjsx-mode web-mode restclient-mode
-                            )))
