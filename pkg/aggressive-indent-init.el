06db1ffe08da5306242af3012b6134d1d1464868
vutran1710 <me@vutr.io>
2018-07-31 10:47:25 +0700

    Refactor aggressive-indent

--- pkg/aggressive-indent-init.el
+++ pkg/aggressive-indent-init.el
@@ -1,3 +1,4 @@
-(require 'aggressive-indent)
-
-(global-aggressive-indent-mode 1)
+(use-package aggressive-indent
+  :ensure t
+  :hook (after-init . global-aggressive-indent-mode)
+  :config (add-hook 'scala-mode-hook (lambda () (aggressive-indent-mode -1))))
