614393f08a6eabcc50350a184522605b0c7320ec
vutran1710 <me@vutr.io>
2018-08-03 14:37:41 +0700

    Refactor elpy

--- pkg/elpy-init.el
+++ pkg/elpy-init.el
@@ -1,3 +1,8 @@
-(require 'elpy)
-(elpy-enable)
-(add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1)))
+(use-package elpy
+  :ensure t
+  :config
+  (elpy-enable)
+  (add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1)))
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args "-i --simple-prompt"
+        elpy-shell-use-project-root nil))
