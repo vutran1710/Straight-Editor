ca7640bdcda51b27a81503d5bdd2e9f1f78e2f08
vutran1710 <me@vutr.io>
2018-08-04 12:49:55 +0700

    Add keychord switch-buffer

--- pkg/keychord-init.el
+++ pkg/keychord-init.el
@@ -4,4 +4,6 @@
   (key-chord-mode 1)
   (setq key-chord-two-keys-delay 0.1)
   (setq key-chord-one-key-delay 0.2)
-  (key-chord-define-global "km" 'volatile-kill-buffer))
+  (key-chord-define-global "km" 'volatile-kill-buffer)
+  (key-chord-define-global "fj" 'ivy-switch-buffer)
+  (key-chord-define-global "vc" 'magit-branch-and-checkout))
