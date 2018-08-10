b55864090954ae0bcc8b4a87b418873b68fc1d1b
vutran1710 <me@vutr.io>
2018-08-10 09:58:53 +0700

    Update elpy

--- pkg/elpy-init.el
+++ pkg/elpy-init.el
@@ -5,4 +5,6 @@
   (add-hook 'elpy-mode-hook (lambda () (highlight-indentation-mode -1)))
   (setq python-shell-interpreter "ipython"
         python-shell-interpreter-args "-i --simple-prompt"
-        elpy-shell-use-project-root nil))
+        elpy-shell-use-project-root nil
+        elpy-set-test-runner "pytest"
+        elpy-test-pytest-runner-command '("pytest" "-s" "--service=talentmind" "--env=local")))
