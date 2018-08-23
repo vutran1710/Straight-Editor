0dbda1667673b21c24a6b179cdb2c146805de0b9
vutran1710 <me@vutr.io>
2018-08-23 17:12:22 +0700

    Fix restclient

--- pkg/restclient-init.el
+++ pkg/restclient-init.el
@@ -1,7 +1,15 @@
 (use-package restclient
   :ensure t
-  :hook (restclient-mode-hook . linum-mode)
+  :init
+  (use-package know-your-http-well :ensure t)
+  (use-package company-restclient :ensure t)
+  :hook
+  (restclient-mode . linum-mode)
   :config
-  (add-to-list 'auto-mode-alist '("\\.api\\'" . restclient-mode))
+  (set (make-local-variable 'company-backends)
+       '(company-dabbrev-code company-dabbrev company-restclient company-semantic company-files
+                              (company-dabbrev-code company-keywords)
+                              (company-dabbrev company-capf company-keywords)))
   (add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode))
+  (add-to-list 'auto-mode-alist '("\\.api\\'" . restclient-mode))
   (add-to-list 'auto-mode-alist '("\\.rest\\'" . restclient-mode)))
