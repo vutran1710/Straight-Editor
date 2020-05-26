47c254d11f60f873181dfada3be71d41838af565
vutran1710 <me@vutr.io>
2020-05-26 12:24:56 +0700

    fix cider

--- lang-ide/cider-init.el
+++ lang-ide/cider-init.el
@@ -1,3 +1,30 @@
+;;; Custom config function
+(defun custom-clj-mode-hook ()
+  "Hook on clojure-mode."
+  (yas-reload-all)
+                    (yas-minor-mode)
+                    (eldoc-mode)
+                    (aggressive-indent-mode)
+                    (clj-refactor-mode)
+                    (add-hook 'before-save-hook #'clj-mode-before-save-hook))
+
+(defun config-cider ()
+  "Setup cider."
+  (setq clojure-indent-style 'align-arguments)
+  (setq clojure-align-forms-automatically t)
+  (setq cider-repl-pop-to-buffer-on-connect nil)
+  (setq cider-repl-use-pretty-printing t)
+  (setq
+   cider-repl-history-file ".cider-repl-history"
+   nrepl-log-messages t)
+  (flycheck-clojure-setup))
+
+(defun clj-mode-before-save-hook ()
+  "Format clj file before save hook."
+  (when (eq major-mode 'clojure-mode)
+    (cider-format-buffer)))
+
+;;; Install packages
 (use-package aggressive-indent
   :ensure t)
 
@@ -5,43 +32,29 @@
   :ensure t)
 
 (use-package flycheck-clojure
+  :ensure t
   :after flycheck
   :defer t
-  ;; :commands (flycheck-clojure-setup)               ;; autoload
+  :commands (flycheck-clojure-setup)
   :config
   (eval-after-load 'flycheck
     '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
 
-(use-package flycheck-pos-tip :ensure t
+(use-package flycheck-pos-tip
+  :ensure t
   :after flycheck)
-
-(defun clj-mode-before-save-hook ()
-  (when (eq major-mode 'clojure-mode)
-    (cider-format-buffer)))
 
 (use-package cider
   :ensure t
+  :defer t
   :hook
-  (clojure-mode . (lambda()
-                    (yas-reload-all)
-                    (yas-minor-mode)
-                    (eldoc-mode)
-                    (aggressive-indent-mode)
-                    (add-hook 'before-save-hook #'clj-mode-before-save-hook)))
-  :config
-  (setq clojure-indent-style 'align-arguments)
-  (setq clojure-align-forms-automatically t)
-  (setq cider-repl-pop-to-buffer-on-connect nil)
-  (setq
-   cider-repl-history-file ".cider-repl-history"  ;; not squiggly-related, but I like it
-   nrepl-log-messages t))
+  (clojure-mode . custom-clj-mode-hook)
+  :config (config-cider))
 
 (use-package clj-refactor
   :ensure t
   :diminish clj-refactor-mode
-  :hook (clojure-mode . clj-refactor-mode)
-  :config
-  (cljr-add-keybindings-with-prefix "C-c C-m"))
+  :bind (("C-c C-m" . cljr-add-keybindings-with-prefix)))
 
 (use-package kibit-helper
   :ensure t
