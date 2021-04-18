119a8c74790a5f19cfc7f850b1c87fca2037c19f
vutran1710 <me@vutr.io>
2021-04-18 23:34:18 +0700

    Add more keybindings

--- lisp/keybindings.el
+++ lisp/keybindings.el
@@ -3,6 +3,9 @@
 ;;; my own key-bindings
 ;;; Code:
 
+(global-set-key (kbd "C-c h") 'recenter)
+(global-set-key (kbd "M-z") 'zap-up-to-char)
+
 (global-set-key (kbd "M-m") 'execute-extended-command)
 (global-set-key (kbd "C-s") 'swiper)
 (global-set-key (kbd "C-c m") 'projectile-vc)
@@ -15,7 +18,10 @@
 (global-set-key (kbd "C-t") 'frog-jump-buffer)
 (global-set-key (kbd "C-c r") 'recentf-open-files)
 
-(global-set-key (kbd "C-'") 'avy-goto-char-2)
+(global-set-key (kbd "C-c a j") 'avy-goto-line)
+(global-set-key (kbd "C-c a k") 'avy-goto-char-2)
+(global-set-key (kbd "C-c a l") 'avy-goto-line)
+
 (global-set-key (kbd "C-]") 'er/expand-region)
 
 (global-set-key (kbd "C-c i e") 'iedit-mode)
@@ -30,7 +36,14 @@
 
 (global-set-key (kbd "C-c s r") 'sp-rewrap-sexp)
 (global-set-key (kbd "C-c s u") 'sp-unwrap-sexp)
+(global-set-key (kbd "C-c s j") 'sp-slurp-hybrid-sexp)
+(global-set-key (kbd "C-M-n") 'sp-end-of-sexp)
+(global-set-key (kbd "C-M-p") 'sp-beginning-of-sexp)
 (global-set-key (kbd "C-c s t") 'smartparens-strict-mode)
+
+(global-set-key (kbd "M-u") #'fix-word-upcase)
+(global-set-key (kbd "M-l") #'fix-word-downcase)
+(global-set-key (kbd "M-c") #'fix-word-capitalize)
 
 (global-set-key (kbd "C-c q") 'emmet-expand-yas)
 
