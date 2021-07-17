c5d6a4ec17b17059c5f75026b8ed4909aa224c75
vutran1710 <me@vutr.io>
2021-07-17 22:10:00 +0700

    adjust python setup

--- lisp/vutr-python.el
+++ lisp/vutr-python.el
@@ -3,6 +3,25 @@
 ;;; python setup
 ;;; Code:
 
+;; Custom tweaks
+(defun setup-checkers (python-bin-path)
+  "Setup checkers path depends on the location of virtualenv dir."
+  (require 'flycheck)
+  (setq flycheck-python-flake8-executable python-bin-path)
+  (setq flycheck-python-pylint-executable python-bin-path)
+  (flycheck-add-next-checker 'python-flake8 'python-pylint)
+  (flycheck-add-next-checker 'python-pylint 'python-mypy))
+
+(defun find-venv-dir-and-setup-checkers ()
+  "Look for .venv dir.  If exists, setup flycheck-checkers."
+  (require 'projectile)
+  (let ((venv-path (concat (projectile-project-root) ".venv")))
+    (if (f-exists? venv-path)
+        (progn
+          (setup-checkers (concat venv-path "/bin/python3"))))))
+
+
+;; Install stuffs
 (use-package poetry
   :ensure t
   :config
@@ -11,35 +30,27 @@
 (use-package pyvenv
   :ensure t
   :diminish
+  :hook
+  (python-mode . (lambda()
+                   (blacken-mode)
+                   (find-venv-dir-and-setup-checkers)))
   :config
   (pyvenv-mode +1)
-  (pyvenv-tracking-mode +1)
-  (setenv "WORKON_HOME" "/Users/vutran/.local/share/virtualenvs/"))
+  (pyvenv-tracking-mode +1))
 
 (use-package blacken
-  :ensure t
-  :hook ((python-mode . blacken-mode)))
+  :ensure t)
 
 (use-package lsp-python-ms
   :ensure t
   :init
   (setq lsp-python-ms-auto-install-server t)
+  (setq lsp-diagnostics-provider :none)
   (add-to-list 'auto-mode-alist '("\\Pipfile\\'" . conf-unix-mode))
-  (setq pyvenv-post-activate-hooks
-	(lambda()
-	  (message "activated...")
-	  (setq lsp-python-ms-extra-paths
-		(vector
-		 (projectile-project-root)
-		 (concat pyvenv-virtual-env "lib/python3.7/site-packages/")
-		 (concat pyvenv-virtual-env "lib/python3.8/site-packages/")
-		 (concat pyvenv-virtual-env "lib/python3.9/site-packages/")))
-	  (flycheck-add-next-checker 'lsp 'python-flake8)
-	  (flycheck-add-next-checker 'python-flake8 'python-mypy)))
   :hook
   (python-mode . (lambda ()
-		   (require 'lsp-python-ms)
-		   (lsp-deferred))))
+                   (require 'lsp-python-ms)
+                   (lsp-deferred))))
 
 
 (provide 'vutr-python)
