f101b11f12f02aaf023356a36d9f981293f2d194
vutran1710 <me@vutr.io>
2022-08-15 11:18:08 +0700

    always fmt

--- lisp/vutr-go.el
+++ lisp/vutr-go.el
@@ -24,6 +24,7 @@
                      (setq-default indent-tabs-mode nil)
                      (setq-default tab-width 4)
                      (setq indent-line-function 'insert-tab)
+                     (add-hook 'before-save-hook #'gofmt)
                      )))
 
 
