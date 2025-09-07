0e80b2076b61d78b648bd88986fb2371be7f0bcd
Vu Tran <me@vutr.io>
2025-09-07 21:01:32 +0700

    Refactor rust setup for Emacs 30

--- lisp/vutr-rust.el
+++ lisp/vutr-rust.el
@@ -3,15 +3,26 @@
 ;;; for Rust
 ;;; Code:
 
+(eval-when-compile
+  (require 'use-package))
+
+(defun vutr-rust--maybe-disable-flycheck ()
+  "Disable `flycheck-mode' when using `eglot'."
+  (when (bound-and-true-p eglot--managed-mode)
+    (flycheck-mode -1)))
+
 (use-package rustic
   :ensure t
+  :hook (rustic-mode . (lambda ()
+                         (eglot-ensure)
+                         (vutr-rust--maybe-disable-flycheck)))
   :init
   (setq rustic-lsp-client 'eglot)
   :bind (:map rustic-mode-map
-              ("C-c C-c l" . flycheck-list-errors))
+              ("C-c C-c l" . flymake-show-project-diagnostics))
   :config
-  (setq rustic-format-on-save t)
-  (setq rustic-rustfmt-args "+nightly")
+  (setq rustic-format-on-save t
+        rustic-rustfmt-args "+nightly")
   (setenv "RUST_LOG" "info"))
 
 
