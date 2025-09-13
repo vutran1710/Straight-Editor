217276e4cd3d8f0e1d610016731fbc1d69513a03
vutran1710 <me@vutr.io>
2025-09-13 17:30:59 +0700

    fix rust

--- lisp/vutr-rust.el
+++ lisp/vutr-rust.el
@@ -8,17 +8,30 @@
   :init
   (setq rust-mode-treesitter-derive t)
   (setq rustic-lsp-client 'eglot)
-
-  :bind (:map rustic-mode-map
-              ("C-c C-c l" . flycheck-list-errors))
-
-  :config
-  (setq rustic-format-on-save t)
-  (setq rustic-rustfmt-args "+nightly")
+  (setq rustic-format-on-save nil)                  ;; <— disable direct rustfmt
+  (setq rustic-rustfmt-args "--edition 2024")       ;; harmless if using Eglot
   (setenv "RUST_LOG" "info")
-
   :custom
-  (rustic-analyzer-command '("rustup" "run" "stable" "rust-analyzer")))
+  (rustic-analyzer-command '("rustup" "run" "stable" "rust-analyzer"))
+  :hook
+  ((rustic-mode . eglot-ensure)
+   (eglot-managed-mode . (lambda ()
+                           (add-hook 'before-save-hook #'eglot-format-buffer -10 t)))
+   (rustic-mode . (lambda () (setq-local compile-command "cargo check"))))
+  :bind
+  (:map rustic-mode-map
+        ("C-c C-c l" . (lambda ()
+                         (interactive)
+                         (if (fboundp 'flymake-show-buffer-diagnostics)
+                             (flymake-show-buffer-diagnostics)
+                           (message "Flymake UI not available."))))
+        ("C-c C-c a" . eglot-code-actions)
+        ("C-c C-c r" . eglot-rename)
+        ("C-c C-c d" . eldoc)
+        ("C-c C-c c" . rustic-cargo-check)
+        ("C-c C-c t" . rustic-cargo-test)
+        ("C-c C-c b" . rustic-cargo-build)))
+
 
 
 (provide 'vutr-rust)
