be4f1bca4b294b631146a3ff2a0757e3db927b6b
vutran1710 <me@vutr.io>
2025-09-27 10:12:21 +0700

    feat(ui): replace mood-line with lambda-line for enhanced mode line

--- lisp/global-packages-setup.el
+++ lisp/global-packages-setup.el
@@ -207,11 +207,24 @@
   (beacon-mode 1))
 
 ;; Minimal mode line
-(use-package mood-line
-  :ensure t
-  :defer nil
-  :init
-  (mood-line-mode 1))
+(use-package lambda-line
+  :straight (:type git :host github :repo "lambda-emacs/lambda-line")
+  :custom
+  (lambda-line-icon-time t) ;; requires ClockFace font (see below)
+  (lambda-line-clockface-update-fontset "ClockFaceRect") ;; set clock icon
+  (lambda-line-abbrev t) ;; abbreviate major modes
+  (lambda-line-hspace "  ")  ;; add some cushion
+  (lambda-line-prefix t) ;; use a prefix symbol
+  (lambda-line-prefix-padding nil) ;; no extra space for prefix
+  (lambda-line-status-invert nil)  ;; no invert colors
+  (lambda-line-gui-ro-symbol  " ⨂") ;; symbols
+  (lambda-line-gui-mod-symbol " ⬤")
+  (lambda-line-gui-rw-symbol  " ◯")
+  (lambda-line-space-top +.50)  ;; padding on top and bottom of line
+  (lambda-line-space-bottom -.50)
+  (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
+  :config
+  (lambda-line-mode))
 
 ;;;;; Version Control and Projects
 
